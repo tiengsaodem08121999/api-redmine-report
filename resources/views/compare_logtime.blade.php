e347291f3c4e58a9b0d38ee29a710ec1aeb6e5b2
QuyLV <quylv@splus-software.com>
2025-06-02 14:53:50 +0700

    update compare logtime

--- resources/views/compare_logtime.blade.php
+++ resources/views/compare_logtime.blade.php
@@ -5,16 +5,20 @@
 </head>
 <body>
     <h1>Compare Logtime</h1>
-    @foreach ($LogtimeMatchReport as $date => $members)
-        <h2>{{ $date }}</h2>
-        @foreach ($members as $member => $errors)
-            <p><strong>{{ $member }}</strong></p>
-            <ul>
-                @foreach ($errors as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    @if(isset($LogtimeMatchReport))
+        @foreach ($LogtimeMatchReport as $date => $members)
+            <h2>{{ $date }}</h2>
+            @foreach ($members as $member => $errors)
+                <p><strong>{{ $member }}</strong></p>
+                <ul>
+                    @foreach ($errors as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endforeach
         @endforeach
-    @endforeach
+    @else
+        <p>Hong có sai gì hết é.</p>
+    @endif
 </body>
 </html> 
