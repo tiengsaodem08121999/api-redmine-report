47ee8ee0b8f7faaafc5f4ad5a12b25eb2fdcb0e5
QuyLV <quylv@splus-software.com>
2025-06-02 16:30:21 +0700

    update member

--- config/information.php
+++ config/information.php
@@ -3,15 +3,15 @@
         'assignee' => [
             'DuongNT' => 758,
             'KietNA' => 1297,
-            // 'AnTNM' => 1300,
+            'AnTNM' => 1300,
             'ChuongNPN' => 1310,
             'QuyLV' => 1261,
-            // 'DuyTT' => 1381,
-            // 'HungBT' => 1283,
-            // 'HaoNV' => 1365,
+            'DuyTT' => 1381,
+            'HungBT' => 1283,
+            'HaoNV' => 1365,
             'YenNH' => 1402,
             'PhuDT' => 996,
-            // 'ThienND' => 1416,
+            'ThienND' => 1416,
         ],
         'tracker' => [
             'Task' => 5,
@@ -22,9 +22,9 @@
             'QuyLV'   => 'cac020bc0f405ab33aba64abffe5216beacf4a27',
             'DuongNT' => 'cad5eb98b070c1ee75330031c0e34ed4cd412eb1',
             'KietNA'  => 'ece23009da9170c5d5edb0c1ef1095db5657f9f4',
-            // 'ThienND' => '81baff340b161e0c9799bfda7268c01afaed772b',
-            // 'NganPVH' => 'cc513ebd6bc00327f89c368199fc8fc928de8a97',
-            // 'BaoNC'   => 'b39e883aebf737c58ed286f4c2a909d5769f19a9',
+            'ThienND' => '81baff340b161e0c9799bfda7268c01afaed772b',
+            'NganPVH' => 'cc513ebd6bc00327f89c368199fc8fc928de8a97',
+            'BaoNC'   => 'b39e883aebf737c58ed286f4c2a909d5769f19a9',
         ],
         'developer_report' => [
             'VinhDV', 
@@ -33,10 +33,10 @@
             'DuongNT', 
             'PhuDT', 
             'YenNH',
-            // 'ThienND',
+            'ThienND',
             'ChuongNPN', 
-            // 'BaoNC', 
-            // 'DuyTT', 
-            // 'NganPVH'
+            'BaoNC', 
+            'DuyTT', 
+            'NganPVH'
         ],
 ];
